23f3f167409df39d5ac115c14139ecdfe8b652b9
maalin2 <maalin.2@osu.edu>
2024-05-05 20:26:08 -0400

    map-1

---
+++ java/Map-1/mapAB4.java
@@ -0,0 +1,23 @@
+import java.util.Math;
+
+public Map<String, String> mapAB4(Map<String, String> map) {
+	if (map.containsKey("a") && map.containsKey("b")) {
+		String a = map.get("a");
+		String b = map.get("b");
+
+		if (a.length() == b.length()) {
+			map.put("a", "");
+			map.put("b", "");
+		} else {
+			map.put("c", a);
+			if (b.length() > a.length()) {
+				map.put("c", b);
+			}
+		}
+	}
+
+
+	return map;  
+}
+
+
